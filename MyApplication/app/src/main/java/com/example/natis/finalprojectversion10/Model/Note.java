e32b6dc710f7ee0a77929fc76d3dc67a1905d4d2
natisadeh <nati.sadeh@gmail.com>
2017-09-03 14:29:16 +0300

    first

---
+++ MyApplication/app/src/main/java/com/example/natis/finalprojectversion10/Model/Note.java
@@ -0,0 +1,12 @@
+package com.example.natis.finalprojectversion10.Model;
+
+public class Note {
+    private String note;
+
+    public Note() {
+    }
+
+    public String getNote() {return note;}
+
+    public void setNote(String note) {this.note = note;}
+}
